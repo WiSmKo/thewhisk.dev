34b33cb86b15f394d7518cecdc7f1a4114357f2a
William Smallman-Köpf <smallmankoepf@gmail.com>
2021-01-20 12:18:42 +0000

    beta

--- components/footer.js
+++ components/footer.js
@@ -5,15 +5,20 @@ class Footer extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-        <footer class="pt-5 noprint">
+        <footer class="pt-5 d-print-none">
             <div class="container-fluid">
                 <p>&#169;2021 William Smallman-K&#246;pf</p>
                 <p><a href="mailto:william@thewhisk.dev">william@thewhisk.dev</a></p>
+                <p>The WHISK web design and web development services</p>
                 <p>Birmingham, UK</p>
             </div>
         </footer> 
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
       `;
     }
   }
   
   customElements.define('footer-component', Footer);
+  
--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ class Header extends HTMLElement {
 
 connectedCallback() {
     this.innerHTML = `
-        <div class="container-fluid noprint">
+        <div class="container-fluid d-print-none">
 
         <div class="row">
         <a href="index.html"><img id = "headLogo" src="images/Logo.png" class="mx-auto d-block" alt="the whisk logo"></a>
